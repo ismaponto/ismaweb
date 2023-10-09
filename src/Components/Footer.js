e1149c6ca97844316a3260111da088488ec4b441
ismaponto <isma.pontoriero@gmail.com>
2023-10-09 10:33:21 +0200

    google analitycs

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,20 +4,20 @@ import linkedinico from "./ico/linkedin.png"
 
 function Footer() {
     return (
-      <footer className="flex bg-white rounded-lg shadow m-2 dark:bg-gray-800 p-8 ">
+      <footer className="flex align-items-center rounded-lg shadow m-2 dark:bg-gray-800 p-8 ">
         <div className="mx-auto max-w-screen-xl p-4 sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          <span className="text-sm text-gray-500 pr-2 sm:text-center dark:text-gray-400">
             © 2023 <a href="#Contacto" className="hover:underline">developed by </a> Ismael Pontoriero.
           </span>
-          <ul className="flex flex-wrap items-center font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-            <li className=" flex max-h-2 p-4">
+          <ul className="flex flex-row flex-wrap items-justify-center font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li className=" flex pr-2">
               <a href="https://www.instagram.com/ismaponto/" className="hover:underline">
-                <img src={instagramico} alt="Instagram" className="w-8 hover:bg-blue-800" />
+                <img src={instagramico} alt="Instagram" className="w-8 hover:bg-teal-500" />
               </a>
             </li>
-            <li className="flex max-h-2 p-4 ">
+            <li className="flex">
               <a href="https://www.linkedin.com/in/ismael-pontoriero-39122026/" className="mr-4 hover:underline md:mr-6">
-                <img src={linkedinico} alt="LinkedIn" className="w-8 hover:bg-blue-800" />
+                <img src={linkedinico} alt="LinkedIn" className="w-8 hover:bg-teal-500" />
               </a>
             </li>
            
